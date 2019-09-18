353b5b1d56aa49df42e132269c51ec8518e25fb2
Steven <parrsm18@wfu.edu>
2019-09-17 20:04:48 -0400

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <assert.h>
 using namespace std;
 
 // function declarations
@@ -16,7 +17,11 @@ int main() {
     vector<int> v(length);  // vector to be sorted
     vector<int> t(length);  // temporary workspace
     // unit test for merge
+
+
     /* your code here */
+
+
     // initialize and print input
     for(int i = 0; i < v.size(); i++) {
         v.at(i) = rand() % 100;
@@ -25,7 +30,7 @@ int main() {
     cout << endl;
 
     // sort v
-    /* your code here */
+    mergeSort(v, t,0, v.size() - 1);
 
     // print output
     for(int i = 0; i < v.size(); i++) {
@@ -35,10 +40,56 @@ int main() {
 
     // check output, make sure it's sorted
     for(int i = 1; i < v.size(); i++) {
-        assert(v.at(i-1) <= v.at(i));
+        assert (v.at(i-1) <= v.at(i));
     }
 
     return 0;
+}
+// Merge Sort divides up into smaller
+// mergesortedlists adds them up together
+
+void mergeSort(vector<int>& a, vector<int>& tmp, int left, int right){
+    if (right <= left){
+        return;
+    }
+    else {
+        int midpoint = (right + left) / 2;
+        mergeSort(a, tmp, left, midpoint);
+        mergeSort(a, tmp, midpoint + 1, right);
+        mergeSortedLists(a, tmp, left, midpoint, right);
+
+    }
+}
+
+void mergeSortedLists(vector<int>& a, vector<int>& tmp, int left, int middle, int right){
+    int temp = left;
+    int tempLeft = left;
+    int tempRight = middle + 1;
+    while (tempLeft <= middle && tempRight <= right){
+        if(a.at(tempLeft) > a.at(tempRight)){
+            tmp.at(temp) = a.at(tempRight);
+            temp++;
+            tempRight++;
+        }
+        else {
+            tmp.at(temp) = a.at(tempLeft);
+            temp++;
+            tempLeft++;
+        }
+    }
+    while (tempRight <= right){
+        tmp.at(temp) = a.at(tempRight);
+        temp++;
+        tempRight++;
+    }
+    while (tempLeft <= middle){
+        tmp.at(temp) = a.at(tempLeft);
+        temp++;
+        tempLeft++;
+    }
+    for (int i = left; i <= right; i++){
+        a.at(i) = tmp.at(i);
+    }
 }
 
 /* your code here */
